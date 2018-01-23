680f4354982f6b3dbf154dd3da2f9b9efcf1b42d
jinhuizxc <1004260403@qq.com>
2018-01-23 18:07:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jinhui/androiddemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.jinhui.androiddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 2018/1/23,
+ * 最近在学习自定义篇，启舰大神的文章，
+ * 想着以前自己也有很多Android笔记，这么久了，
+ * 可以整理一篇合集，做到温故而知新！
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
